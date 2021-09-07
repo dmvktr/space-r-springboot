4af0cd7ffb1c591e953d3ba342c4408a8362fd8b
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-07 19:20:00 +0200

    [update] DataHandlerService constructor for Astronaut dao

--- src/main/java/com/codecool/service/DataHandlerService.java
+++ src/main/java/com/codecool/service/DataHandlerService.java
@@ -1,14 +1,17 @@
 package com.codecool.service;
 
 import com.codecool.dao.ArticleDao;
+import com.codecool.dao.AstronautDao;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataHandlerService {
     private ArticleDao articleDao;
+    private AstronautDao astronautDao;
 
-    public DataHandlerService(ArticleDao articleDao) {
+    public DataHandlerService(ArticleDao articleDao, AstronautDao astronautDao) {
         this.articleDao = articleDao;
+        this.astronautDao = astronautDao;
     }
 
     public ArticleDao getArticleDao() {
@@ -17,5 +20,13 @@ public class DataHandlerService {
 
     public void setArticleDao(ArticleDao articleDao) {
         this.articleDao = articleDao;
+    }
+
+    public AstronautDao getAstronautDao() {
+        return astronautDao;
+    }
+
+    public void setAstronautDao(AstronautDao astronautDao) {
+        this.astronautDao = astronautDao;
     }
 }
