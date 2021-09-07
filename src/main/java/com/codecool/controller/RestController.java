5c741cc8133ca86e23cb9806c7c0f03c4b10a43e
viktor <v.dome1991@gmail.com>
2021-09-07 17:48:05 +0200

    [update] rest controller with cors header

--- src/main/java/com/codecool/controller/RestController.java
+++ src/main/java/com/codecool/controller/RestController.java
@@ -9,12 +9,14 @@ import com.codecool.service.apiAccessRoutes.APIAccessRoutes;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
     private APIDataHandler apiDataHandler;
     private DataHandlerService dataService;
+    private String CORS_POLICY = "http://localhost:3000";
 
     public RestController(APIDataHandler apiDataHandler, DataHandlerService dataService) {
         this.apiDataHandler = apiDataHandler;
@@ -23,7 +25,8 @@ public class RestController {
 
     @ResponseBody
     @GetMapping("/news")
-    public List<Article> news() {
+    public List<Article> news(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", CORS_POLICY);
         ArticleDao articleDao = dataService.getArticleDao();
         List<Article> articles = apiDataHandler.fetchData(APIAccessRoutes.NEWS);
         articleDao.updateNews(articles);
