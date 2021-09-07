195eee8898f98ee3506f6d43f267a35ca1caed99
viktor <v.dome1991@gmail.com>
2021-09-07 09:57:35 +0200

    [add] base project structure

---
+++ src/main/java/com/codecool/SpaceRSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.codecool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceRSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceRSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/SpaceRSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceRSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
