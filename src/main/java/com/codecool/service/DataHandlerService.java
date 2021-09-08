76179e52058902cda214877d5db528c591e8e623
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-09-08 12:08:05 +0200

    [add] event dao to constructor and getter setter methods

--- src/main/java/com/codecool/service/DataHandlerService.java
+++ src/main/java/com/codecool/service/DataHandlerService.java
@@ -2,17 +2,20 @@ package com.codecool.service;
 
 import com.codecool.dao.ArticleDao;
 import com.codecool.dao.AstronautDao;
+import com.codecool.dao.EventDao;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataHandlerService {
     private ArticleDao articleDao;
-
     private AstronautDao astronautDao;
-    public DataHandlerService(ArticleDao articleDao, AstronautDao astronautDao) {
+    private EventDao eventDao;
+
+    public DataHandlerService(ArticleDao articleDao, AstronautDao astronautDao, EventDao eventDao) {
         this.articleDao = articleDao;
         this.astronautDao = astronautDao;
+        this.eventDao = eventDao;
     }
 
     public ArticleDao getArticleDao() {
@@ -31,4 +34,11 @@ public class DataHandlerService {
         this.astronautDao = astronautDao;
     }
 
+    public EventDao getEventDao() {
+        return eventDao;
+    }
+
+    public void setEventDao(EventDao eventDao) {
+        this.eventDao = eventDao;
+    }
 }
