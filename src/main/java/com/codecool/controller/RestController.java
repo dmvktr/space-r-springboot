d0a900876724d535c776712bdb4f7f8feb004075
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-08 09:45:15 +0200

    [add] Spacecrafts route mapping controller

--- src/main/java/com/codecool/controller/RestController.java
+++ src/main/java/com/codecool/controller/RestController.java
@@ -3,7 +3,9 @@ package com.codecool.controller;
 
 import com.codecool.dao.ArticleDao;
 import com.codecool.dao.AstronautDao;
+import com.codecool.dao.SpacecraftDao;
 import com.codecool.model.Article;
+import com.codecool.model.spacecrafts.Spacecrafts;
 import com.codecool.model.astronauts.Astronauts;
 
 import com.codecool.service.APIDataHandler;
@@ -44,6 +46,16 @@ public class RestController {
         Astronauts astronauts = apiDataHandler.fetchAstronautsData(APIAccessRoutes.ASTRONAUTS);
         astronautDao.updateAstronauts(astronauts);
         return astronautDao.getAllAstronauts();
+    }
+
+
+    @ResponseBody
+    @GetMapping("/spacecrafts")
+    public Spacecrafts spacecrafts() {
+        SpacecraftDao spacecraftDao = dataService.getSpacecraftDao();
+        Spacecrafts spacecrafts = apiDataHandler.fetchSpacecrafts(APIAccessRoutes.SPACECRAFTS);
+        spacecraftDao.updateSpacecrafts(spacecrafts);
+        return spacecraftDao.getAllSpacecrafts();
     }
 
 }
