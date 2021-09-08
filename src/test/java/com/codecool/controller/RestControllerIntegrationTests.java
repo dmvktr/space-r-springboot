f7352deee41eaaa81ec218034ea11901c8345142
viktor <v.dome1991@gmail.com>
2021-09-08 22:43:14 +0200

    [add] integration test for gallery route

--- src/test/java/com/codecool/controller/RestControllerIntegrationTests.java
+++ src/test/java/com/codecool/controller/RestControllerIntegrationTests.java
@@ -2,6 +2,7 @@ package com.codecool.controller;
 
 import com.codecool.model.astronauts.Astronaut;
 import com.codecool.model.astronauts.Astronauts;
+import com.codecool.model.gallery.GalleryPicture;
 import com.codecool.model.news.News;
 import com.codecool.service.DataHandlerService;
 import org.junit.jupiter.api.Test;
@@ -61,7 +62,25 @@ class RestControllerIntegrationTests {
     }
 
     @Test
-    void gallery() {
+    void galleryRoute_shouldReturnWithStatusOkAndCorrectResponse_whenUserVisitsRoute() throws Exception {
+        GalleryPicture picture1 = new GalleryPicture();
+        GalleryPicture picture2 = new GalleryPicture();
+        String picture2Title = "picture2";
+        String dummyUrl = "http://dummyURL.com";
+        picture2.setTitle(picture2Title);
+        GalleryPicture picture3 = new GalleryPicture();
+        picture3.setUrl(dummyUrl);
+        GalleryPicture[] gallery = {picture1, picture2, picture3};
+
+        Mockito.when(dataHandlerService.getAllPictures()).thenReturn(gallery);
+        mockMvc.perform(MockMvcRequestBuilders
+            .get("/gallery"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$").isArray())
+            .andExpect(jsonPath("$", hasSize(3)))
+            .andExpect(jsonPath("$[1].title", is(picture2Title)))
+            .andExpect(jsonPath("$[2].url", is(dummyUrl)));
     }
 
     @Test
