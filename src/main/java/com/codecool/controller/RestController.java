ed47547bd9aee19a072e48e9769ad523873f0455
viktor <v.dome1991@gmail.com>
2021-09-08 12:09:47 +0200

    [refactor] RestController

--- src/main/java/com/codecool/controller/RestController.java
+++ src/main/java/com/codecool/controller/RestController.java
@@ -1,49 +1,40 @@
 package com.codecool.controller;
 
-
-import com.codecool.dao.ArticleDao;
-import com.codecool.dao.AstronautDao;
 import com.codecool.model.Article;
+import com.codecool.model.GalleryPicture;
 import com.codecool.model.astronauts.Astronauts;
 
 import com.codecool.service.APIDataHandler;
 import com.codecool.service.DataHandlerService;
-import com.codecool.service.apiAccessRoutes.APIAccessRoutes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
+@CrossOrigin("http://localhost:3000")
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
     private APIDataHandler apiDataHandler;
     private DataHandlerService dataService;
-    private final String REQUEST_DOMAIN = "http://localhost:3000";
 
+    @Autowired
     public RestController(APIDataHandler apiDataHandler, DataHandlerService dataService) {
         this.apiDataHandler = apiDataHandler;
         this.dataService = dataService;
     }
 
-    @CrossOrigin(REQUEST_DOMAIN)
-    @ResponseBody
     @GetMapping("/news")
-    public List<Article> news() {
-        ArticleDao articleDao = dataService.getArticleDao();
-        List<Article> articles = apiDataHandler.fetchData(APIAccessRoutes.NEWS);
-        articleDao.updateNews(articles);
-        return articleDao.getAllArticles();
+    public Article[] news() {
+        return dataService.getAllArticles();
     }
 
-    @CrossOrigin(REQUEST_DOMAIN)
-    @ResponseBody
     @GetMapping("/astronauts")
     public Astronauts astronaut() {
-        AstronautDao astronautDao = dataService.getAstronautDao();
-        Astronauts astronauts = apiDataHandler.fetchAstronautsData(APIAccessRoutes.ASTRONAUTS);
-        astronautDao.updateAstronauts(astronauts);
-        return astronautDao.getAllAstronauts();
+        return dataService.getAllAstronauts();
+    }
+
+    @GetMapping("/gallery")
+    public GalleryPicture[] gallery(){
+        return dataService.getAllPictures();
     }
 
 }
