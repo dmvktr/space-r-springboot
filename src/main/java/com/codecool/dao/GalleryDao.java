3586c46fece1dacec1393dd87476861479a6811b
viktor <v.dome1991@gmail.com>
2021-09-08 14:10:35 +0200

    [refactor] GalleryDao method return type

--- src/main/java/com/codecool/dao/GalleryDao.java
+++ src/main/java/com/codecool/dao/GalleryDao.java
@@ -1,13 +1,10 @@
 package com.codecool.dao;
 
-import com.codecool.model.Article;
 import com.codecool.model.GalleryPicture;
-
-import java.util.List;
 
 
 public interface GalleryDao {
-    List<Article> getAllPictures();
+    GalleryPicture[] getAllPictures();
 
     GalleryPicture getBy(int id);
 }
