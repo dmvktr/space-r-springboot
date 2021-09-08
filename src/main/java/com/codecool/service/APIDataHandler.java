3c3cfe2f36b76fd3e504eff7d02aef83222570e9
viktor <v.dome1991@gmail.com>
2021-09-08 14:08:43 +0200

    [add] flux datafetch to APIDataHandler

--- src/main/java/com/codecool/service/APIDataHandler.java
+++ src/main/java/com/codecool/service/APIDataHandler.java
@@ -1,12 +1,30 @@
 package com.codecool.service;
 
 import com.codecool.service.apiAccessRoutes.APIAccessRoutes;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class APIDataHandler {
 
+    private final WebClient webClient;
+
+    public APIDataHandler() {
+        this.webClient = WebClient.create();
+    }
+
     public String fetchAPIRoute(APIAccessRoutes api){
         return api.getApiPath();
+    }
+
+    public <T> T fetchData(APIAccessRoutes targetApi, Class<T> targetType){
+        Mono<T> response = webClient.get()
+            .uri(fetchAPIRoute(targetApi))
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(targetType);
+        return targetType.cast(response.block());
     }
 }
