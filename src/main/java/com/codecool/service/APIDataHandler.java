84bfd83f1fa0da14ef8400ab965e113bcbb4aa17
viktor <v.dome1991@gmail.com>
2021-09-08 11:57:26 +0200

    [refactor] APIDataHandler

--- src/main/java/com/codecool/service/APIDataHandler.java
+++ src/main/java/com/codecool/service/APIDataHandler.java
@@ -1,29 +1,12 @@
 package com.codecool.service;
 
-import com.codecool.model.Article;
-import com.codecool.model.astronauts.Astronauts;
-
 import com.codecool.service.apiAccessRoutes.APIAccessRoutes;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 @Component
 public class APIDataHandler {
 
-    public List<Article> fetchData(APIAccessRoutes api) {
-        RestTemplate restTemplate = new RestTemplate();
-        return Arrays.asList(Objects.requireNonNull(restTemplate
-            .getForObject(api.getApiPath(), Article[].class)));
+    public String fetchAPIRoute(APIAccessRoutes api){
+        return api.getApiPath();
     }
-
-    public Astronauts fetchAstronautsData(APIAccessRoutes api) {
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate
-            .getForObject(api.getApiPath(), Astronauts.class);
-    }
-
 }
