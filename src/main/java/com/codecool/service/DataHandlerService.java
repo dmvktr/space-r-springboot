2ac1837e15ffc4e093e522860fe1591ca0717dc7
viktor <v.dome1991@gmail.com>
2021-09-08 12:08:36 +0200

    [refactor] DataHandlerService

--- src/main/java/com/codecool/service/DataHandlerService.java
+++ src/main/java/com/codecool/service/DataHandlerService.java
@@ -3,32 +3,63 @@ package com.codecool.service;
 import com.codecool.dao.ArticleDao;
 import com.codecool.dao.AstronautDao;
 
+import com.codecool.dao.GalleryDao;
+import com.codecool.model.Article;
+import com.codecool.model.GalleryPicture;
+import com.codecool.model.astronauts.Astronaut;
+import com.codecool.model.astronauts.Astronauts;
+import com.codecool.service.apiAccessRoutes.APIAccessRoutes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Component
 public class DataHandlerService {
     private ArticleDao articleDao;
-
     private AstronautDao astronautDao;
-    public DataHandlerService(ArticleDao articleDao, AstronautDao astronautDao) {
+    private GalleryDao galleryDao;
+    private WebClient webClient;
+    private APIDataHandler apiDataHandler;
+
+    @Autowired
+    public DataHandlerService(@Qualifier("articleDao") ArticleDao articleDao, @Qualifier("astronautDao") AstronautDao astronautDao,
+                              @Qualifier("galleryDao") GalleryDao galleryDao, APIDataHandler apiDataHandler) {
         this.articleDao = articleDao;
         this.astronautDao = astronautDao;
+        this.galleryDao = galleryDao;
+        this.webClient = WebClient.create();
+        this.apiDataHandler = apiDataHandler;
     }
 
-    public ArticleDao getArticleDao() {
-        return articleDao;
+    public Astronauts getAllAstronauts(){
+        Mono<Astronauts> response = webClient.get()
+            .uri(apiDataHandler.fetchAPIRoute(APIAccessRoutes.ASTRONAUTS))
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(Astronauts.class);
+        return response.block();
     }
 
-    public void setArticleDao(ArticleDao articleDao) {
-        this.articleDao = articleDao;
+    public Article[] getAllArticles(){
+        Mono<Article[]> response = webClient.get()
+            .uri(apiDataHandler.fetchAPIRoute(APIAccessRoutes.NEWS))
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(Article[].class);
+        return response.block();
     }
 
-    public AstronautDao getAstronautDao() {
-        return astronautDao;
-    }
-
-    public void setAstronautDao(AstronautDao astronautDao) {
-        this.astronautDao = astronautDao;
+    public GalleryPicture[] getAllPictures(){
+        Mono<GalleryPicture[]> response = webClient.get()
+            .uri(apiDataHandler.fetchAPIRoute(APIAccessRoutes.GALLERY))
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(GalleryPicture[].class);
+        return response.block();
     }
 
 }
