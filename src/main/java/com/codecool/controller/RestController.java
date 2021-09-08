79b02693531a14a8731e083a08c4e25b310e4b8d
viktor <v.dome1991@gmail.com>
2021-09-08 14:15:55 +0200

    [update] RestController dependency

--- src/main/java/com/codecool/controller/RestController.java
+++ src/main/java/com/codecool/controller/RestController.java
@@ -6,7 +6,6 @@ import com.codecool.model.astronauts.Astronauts;
 
 import com.codecool.model.locations.Locations;
 import com.codecool.model.spacecrafts.Spacecrafts;
-import com.codecool.service.APIDataHandler;
 import com.codecool.service.DataHandlerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,12 +14,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @CrossOrigin("http://localhost:3000")
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
-    private APIDataHandler apiDataHandler;
-    private DataHandlerService dataService;
+    private final DataHandlerService dataService;
 
     @Autowired
-    public RestController(APIDataHandler apiDataHandler, DataHandlerService dataService) {
-        this.apiDataHandler = apiDataHandler;
+    public RestController(DataHandlerService dataService) {
         this.dataService = dataService;
     }
 
